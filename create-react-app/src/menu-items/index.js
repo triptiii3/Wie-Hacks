5dea2517dc7622356d1c6406a8a228b386dd235e
Yash Kamboj <yashkamboj@Yashs-MacBook-Air-3.local>
2023-05-21 15:51:06 +0530

    removing unwanted pages
    Signed-off-by: Yash Kamboj <yashkamboj@Yashs-MacBook-Air-3.local>

--- create-react-app/src/menu-items/index.js
+++ create-react-app/src/menu-items/index.js
@@ -1,11 +1,10 @@
 import dashboard from './dashboard';
 import pages from './pages';
-import other from './other';
 
 // ==============================|| MENU ITEMS ||============================== //
 
 const menuItems = {
-  items: [dashboard, pages, other]
+  items: [dashboard, pages]
 };
 
 export default menuItems;
