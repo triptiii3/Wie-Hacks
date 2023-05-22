3bc4f0ada9c7a6025a7a7a696c3895a6ff173d9d
Yash Kamboj <yashkamboj@Yashs-MacBook-Air-3.local>
2023-05-22 10:41:33 +0530

    added .env
    Signed-off-by: Yash Kamboj <yashkamboj@Yashs-MacBook-Air-3.local>

--- create-react-app/src/views/pages/chatPage/index.js
+++ create-react-app/src/views/pages/chatPage/index.js
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
 
-const API_KEY = "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";
+const API_KEY = process.env.REACT_APP_API_KEY
 // "Explain things like you would to a 10 year old learning how to code."
 const systemMessage = { //  Explain things like you're talking to a software professional with 5 years of experience.
   "role": "system", "content": "Explain things like you're talking to a software professional with 2 years of experience."
